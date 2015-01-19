bdd8da0a274578fb21446eb62ad87afd9f4b5a92
Corey Oordt <coreyoordt@gmail.com>
2015-01-19 13:30:21 -0600

    Some 3rd party changes

--- setup.py
+++ setup.py
@@ -1,24 +1,34 @@
+#!/usr/bin/env python
+
 import os
-#from setuptools import setup
 from distutils.core import setup
+from disttest import test
+
 
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
     name='django-feedback',
-    version='0.4',
+    version=__import__('feedback').get_version().replace(' ', '-'),
     description='A pluggable user feedback app',
     author='Jared Forsyth',
     author_email='jabapyth@gmail.com',
-    license='BSD',
+    license='Apache v2',
     url='http://github.com/unaizalakain/django-feedback/',
-    keywords = ['blog', 'django', 'feedback', 'ajax', 'user', 'customer', 'comment'],
-    package_data = {'feedback': ['static/feedback/*', 'static/feedback/images/*', 'CREDITS']},
-    install_requires=['yammy'],
+    keywords=['blog', 'django', 'feedback', 'ajax', 'user', 'customer', 'comment'],
+    package_data={'feedback': ['static/feedback/*.js', 'static/feedback/*.css', 'static/feedback/images/*.png', 'CREDITS']},
     packages=[
         'feedback',
     ],
+    cmdclass={'test': test},
+    options={
+        'test': {
+            'test_dir': ['test'],  # will run all .py files in the tests/ directory
+        }
+    },
+    install_requires=read('requirements.txt'),
+    include_package_data=True,
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
