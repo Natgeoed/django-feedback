888adc8f8d7c376ac8c10293e409573af4865382
Corey Oordt <coreyoordt@gmail.com>
2015-01-19 13:33:04 -0600

    Admin update

--- feedback/admin.py
+++ feedback/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
 from feedback.models import Feedback
 
-admin.site.register(Feedback)
+
+class FeedbackAdmin(admin.ModelAdmin):
+    list_display = ('url', 'subject', 'date_submitted')
+    list_filter = ('subject', )
+    date_hierarchy = 'date_submitted'
+    ordering = ('-date_submitted', )
+    search_fields = ('feedback', 'email', 'url')
+
+admin.site.register(Feedback, FeedbackAdmin)
 
 # vim: et sw=4 sts=4
