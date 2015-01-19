a2350ca7fa3735fb726f2aceb74a050f9b7b8cca
Corey Oordt <coreyoordt@gmail.com>
2015-01-19 13:32:10 -0600

    Removed Site as part of model and created a migration

---
+++ feedback/migrations/0001_initial.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'Feedback'
+        db.create_table(u'feedback_feedback', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('url', self.gf('django.db.models.fields.CharField')(max_length=255)),
+            ('subject', self.gf('django.db.models.fields.CharField')(default='', max_length=255, blank=True)),
+            ('feedback', self.gf('django.db.models.fields.TextField')(default='')),
+            ('email', self.gf('django.db.models.fields.EmailField')(default='', max_length=75, blank=True)),
+            ('date_submitted', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
+        ))
+        db.send_create_signal(u'feedback', ['Feedback'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'Feedback'
+        db.delete_table(u'feedback_feedback')
+
+
+    models = {
+        u'feedback.feedback': {
+            'Meta': {'object_name': 'Feedback'},
+            'date_submitted': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'email': ('django.db.models.fields.EmailField', [], {'default': "''", 'max_length': '75', 'blank': 'True'}),
+            'feedback': ('django.db.models.fields.TextField', [], {'default': "''", 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'subject': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '255', 'blank': 'True'}),
+            'url': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        }
+    }
+
+    complete_apps = ['feedback']
--- feedback/models.py
+++ feedback/models.py
@@ -1,15 +1,33 @@
 from django.db import models
-from django.contrib.sites.models import Site
-from django.utils.translation import ugettext as _
+from django.utils.translation import ugettext_lazy as _
+
+from .settings import SUBJECT_CHOICES
 
 
 class Feedback(models.Model):
-    site = models.ForeignKey(Site, verbose_name=_('site'))
-    url = models.CharField(max_length=255, verbose_name=_('url'))
-    subject = models.CharField(max_length=255, blank=True, null=True,
-            verbose_name=_('subject'))
-    email = models.EmailField(blank=True, null=True, verbose_name=_('email'))
-    text = models.TextField(verbose_name=_('text'))
+    url = models.CharField(
+        max_length=255,
+        verbose_name=_('url'),
+        help_text=_('The URL from which the feedback was given.'))
+    subject = models.CharField(
+        _('subject'),
+        choices=SUBJECT_CHOICES,
+        max_length=255,
+        blank=True,
+        default=''
+    )
+    feedback = models.TextField(
+        _('feedback'),
+        default='')
+    email = models.EmailField(
+        _('e-mail (optional)'),
+        blank=True,
+        default='')
+    date_submitted = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = _('feedback')
+        verbose_name_plural = _('feedback')
 
     def __unicode__(self):
-        return u'{url}: {subject}'.format(url=self.url, subject=self.subject)
+        return u'Feedback on {url}'.format(url=self.url)
