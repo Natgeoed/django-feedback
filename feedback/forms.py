9f54c88cb3e6350950b1403e4464d74d1bb188e8
Corey Oordt <coreyoordt@gmail.com>
2015-01-19 13:33:34 -0600

    Tweaked the views.

--- feedback/forms.py
+++ feedback/forms.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
 from django import forms
-from django.contrib.sites.models import Site
 
 from feedback.models import Feedback
 
+
 class FeedbackForm(forms.ModelForm):
     '''The form shown when giving feedback'''
+
     class Meta:
         model = Feedback
-        #exclude = ('site', 'url')
-
-# vim: et sw=4 sts=4
+        exclude = ('date_submitted', )
+        widgets = {
+            'url': forms.HiddenInput()
+        }
--- feedback/views.py
+++ feedback/views.py
@@ -1,47 +1,53 @@
+from django.template.loader import render_to_string
 from django.views.generic import CreateView
 from django.conf import settings
-from django.http import HttpResponse
-from django.contrib.sites.models import Site
-from django.core.mail import send_mail
-from django.utils.translation import ugettext as _
+from django.core.mail import EmailMessage
 
-import json
-
+from feedback.models import Feedback
 from feedback.forms import FeedbackForm
+from feedback.settings import ALERT_EMAIL, EMAIL_TEMPLATE
 
+
+def send_email(to_list, subject, message, sender=settings.SERVER_EMAIL):
+    msg = EmailMessage(subject, message, sender, to_list)
+    msg.content_subtype = "html"  # Main content is now text/html
+    return msg.send()
 
 
 class FeedbackView(CreateView):
+    model = Feedback
     form_class = FeedbackForm
     template_name = 'feedback/feedback.html'
 
-    def get_form_kwargs(self):
-        kwargs = super(FeedbackView, self).get_form_kwargs()
-        post = kwargs.get('data', {}).copy()
-        post['url'] = self.kwargs['url']
-        post['site'] = Site.objects.get_current().pk
-        kwargs['data'] = post
+    def get_initial(self):
+        kwargs = super(FeedbackView, self).get_initial()
+        if 'url' not in kwargs:
+            referer = self.request.META.get('HTTP_REFERER', 'unknown')
+            kwargs['url'] = self.request.GET.get('url', referer)
         return kwargs
 
     def get_success_url(self):
-        return self.kwargs['url']
+        if 'url' in self.kwargs:
+            return self.kwargs['url']
+        else:
+            return self.request.META['HTTP_REFERER']
 
     def form_valid(self, form):
-        response = super(FeedbackView, self).form_valid(form)
-        if hasattr(settings, 'FEEDBACK_EMAIL'):
+        super(FeedbackView, self).form_valid(form)
+        if ALERT_EMAIL:
             d = form.cleaned_data
             try:
-                send_mail(
-                        'Feedback received: {}'.format(d['subject']),
-                        'email: {} \n\n {}'.format(d['email'], d['text']),
-                        settings.SERVER_EMAIL,
-                        [settings.FEEDBACK_EMAIL],
-                        fail_silently=False,
-                        )
+                send_email(
+                    'Feedback received: {}'.format(d['subject']),
+                    render_to_string(EMAIL_TEMPLATE, {'form': d}),
+                    settings.SERVER_EMAIL,
+                    [settings.FEEDBACK_EMAIL],
+                    fail_silently=False,
+                )
             except:
-                return HttpResponse(json.dumps({'error': _('Failed to send email')}))
-        return HttpResponse(json.dumps({}))
-
-    def form_invalid(self, form):
-        return HttpResponse(json.dumps({'errors': form.errors}))
-
+                pass
+        form_class = self.get_form_class()
+        self.initial = {'url': form.data.get('url')}
+        blank_form = form_class(initial=self.get_initial())
+        ctxt = self.get_context_data(form=blank_form, success=True)
+        return self.render_to_response(ctxt)
