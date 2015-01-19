634a2734a7130549aa7fc8ad42a4cd68ba1fd2a1
Corey Oordt <coreyoordt@gmail.com>
2015-01-19 13:31:09 -0600

    Added a settings file to keep track of settings.

---
+++ feedback/settings.py
@@ -0,0 +1,21 @@
+from django.conf import settings
+
+DEFAULT_SETTINGS = {
+    'ALERT_EMAIL': [],
+    'EMAIL_TEMPLATE': 'feedback/email.html',
+    'SUBJECT_CHOICES': (
+        ("suggestion", "I have a suggestion"),
+        ("kudos", "Kudos - I like something"),
+        ("error", "Something went wrong"),
+        ("lost", "I got stuck"),
+        ("missing", "I can't find something"),
+    ),
+}
+
+USER_SETTINGS = DEFAULT_SETTINGS.copy()
+USER_SETTINGS.update(getattr(settings, 'FEEDBACK_SETTINGS', {}))
+
+if not isinstance(USER_SETTINGS['ALERT_EMAIL'], (list, tuple)):
+    USER_SETTINGS['ALERT_EMAIL'] = [USER_SETTINGS['ALERT_EMAIL']]
+
+globals().update(USER_SETTINGS)
