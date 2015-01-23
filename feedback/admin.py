fd0d23de7c60a945cb2402ea0bcf7aa6008d1435
Corey Oordt <coreyoordt@gmail.com>
2015-01-23 15:49:43 -0600

    Added ability to export a subset via admin action

--- feedback/admin.py
+++ feedback/admin.py
@@ -2,6 +2,27 @@ from django.contrib import admin
 from feedback.models import Feedback, FeedbackNote
 
 
+def export_and_update(modeladmin, request, queryset):
+    from django.http import HttpResponse
+    try:
+        from cStringIO import StringIO
+    except ImportError:
+        from StringIO import StringIO
+    data = queryset.values_list('url', 'subject', 'feedback', 'email', 'date_submitted', 'status')
+    output = StringIO()
+
+    # Header
+    output.write('"url","subject","feedback","email","date submitted","status"\n')
+    for row in data:
+        output.write('"{0}","{1}","{2}","{3}","{4:%Y-%m-%d %H:%M:%S}","{5}"\n'.format(*row))
+    queryset.update(status='exported')
+    response = HttpResponse(output.getvalue(), content_type="text/csv")
+    response['Content-Disposition'] = 'attachment; filename="feedback_export.csv"'
+    output.close()
+    return response
+export_and_update.short_description = "Export selected feedback and change status"
+
+
 class FeedbackNoteInline(admin.TabularInline):
     model = FeedbackNote
     extra = 0
@@ -15,6 +36,7 @@ class FeedbackAdmin(admin.ModelAdmin):
     ordering = ('-date_submitted', )
     search_fields = ('feedback', 'email', 'url')
     inlines = (FeedbackNoteInline, )
+    actions = (export_and_update, )
 
     def extended_fields(self, obj):
         return """<div class="arrow" data-target="#extended-%(id)s">
